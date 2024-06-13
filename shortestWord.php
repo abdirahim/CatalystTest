b4e6c723b3e29d3b220c54d73301c08cce825dca
Abdirahim Mahamoud <abdi.mahamoud@toolstation.com>
2024-06-13 16:24:17 +0300

    latest

--- shortestWord.php
+++ shortestWord.php
@@ -1,1 +1,32 @@
 <?php
+$string = file_get_contents('./input.txt', true);
+$word = "";
+$words = array();
+
+$string = $string . " ";
+
+for($i = 0; $i < strlen($string); $i++){
+    //Split the string into words
+    if($string[$i] != ' '){
+        $word = $word . $string[$i];
+    }
+    else{
+        //Add word to string array words
+        array_push($words, $word);
+        //Make word an empty string
+        $word = "";
+    }
+}
+
+$small = $words[0];
+
+//Determine smallest
+for($k = 0; $k < count($words); $k++){
+    if(strlen($small) > strlen($words[$k]))
+        $small = $words[$k];
+
+}
+
+print("Smallest word: " . $small);
+
+
